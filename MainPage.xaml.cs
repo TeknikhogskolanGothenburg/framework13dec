33e51cb8259df0fa6cc3e94bff77e9940c8960a0
Joakim Wassberg <joakim@arthead.se>
2019-12-13 10:12:50 +0100

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Hamburger2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ContentFrame.Navigate(typeof(HomeView));
+        }
+
+        private void MenuSelected(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+           
+            var item = NavView.SelectedItem as NavigationViewItem;
+            switch(item.Tag)
+            {
+                case "home":
+                    ContentFrame.Navigate(typeof(HomeView));
+                    NavView.Header = "Welcome";
+                    break;
+                case "food":
+                    ContentFrame.Navigate(typeof(FoodView));
+                    NavView.Header = "The Food Page";
+                    break;
+                case "users":
+                    ContentFrame.Navigate(typeof(UserView));
+                    NavView.Header = "Users";
+                    break;
+            }
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamburger2.Models
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Image { get; set; }
+    }
+}
